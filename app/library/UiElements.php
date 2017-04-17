a6e3ce6837a357984de077766efc30116fa24c31
mrobayo <mrobayo@hotmail.com>
2017-04-17 13:15:37 -0500

    fix session menu

--- app/library/UiElements.php
+++ app/library/UiElements.php
@@ -98,6 +98,8 @@ class UiElements extends Component
         // session
 		if ($auth) {
         	$this->_headerMenu['navbar-right']['session'] = ['caption' => '<i class="fa '. ($auth['is_admin'] ? 'fa-user': 'fa-user-o') .'"></i> '.$auth['nombre'], 'action' => 'end'];
+        	$this->_headerMenu['navbar-right']['session']['items'][] = ['caption'=> '<i class="fa fa-sign-out"></i> Salir', 'action'=>'end'];
+
         }
 
         $controllerName = $this->view->getControllerName();
